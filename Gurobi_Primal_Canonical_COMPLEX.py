aa3844ea8388c1c8680c5ed277745746d7bc606e
James Gaboardi <jgaboardi@fsu.edu>
2015-08-22 21:10:21 -0400

    update
    update

--- Gurobi_Primal_Canonical_COMPLEX.py
+++ Gurobi_Primal_Canonical_COMPLEX.py
@@ -19,110 +19,127 @@ Adapted from:
 import numpy as np
 import gurobipy as gbp
 import datetime as dt
+import time
 
-#  Constants
-Aij = np.random.randint(5, 50, 250000)
-Aij = Aij.reshape(500,500)
-AijSum = np.sum(Aij)
-Oij = np.random.randint(5, 10, 250000)
-Oij = Oij.reshape(500,500)
-OijSum = np.sum(Oij)
-Cj = np.random.randint(10, 20, 500)
-CjSum = np.sum(Cj)
-Ej = np.random.randint(10, 20, 500)
-EjSum = np.sum(Ej)
-Gj = np.random.randint(10, 20, 500)
-GjSum = np.sum(Gj)
-Bi = np.random.randint(10, 20, 500)
-BiSum = np.sum(Bi)
+def GbpPrimCanCOMPLEX():
 
-# Matrix Shape
-rows = range(len(Aij))
-cols = range(len(Aij[0]))
-
-# Instantiate Model
-mPrimal_Canonical_GUROBI = gbp.Model(' -- Canonical Primal Linear Programming Problem -- ')
-
-# Set Focus to Optimality
-gbp.setParam('MIPFocus', 2)
-
-# Decision Variables
-desc_var = []
-for dest in cols:
-    desc_var.append([])
-    desc_var[dest].append(mPrimal_Canonical_GUROBI.addVar(vtype=gbp.GRB.CONTINUOUS, 
-                                    name='y'+str(dest+1)))                                   
-o_var = []
-for dest in cols:
-    o_var.append([])
-    o_var[dest].append(mPrimal_Canonical_GUROBI.addVar(vtype=gbp.GRB.CONTINUOUS, 
-                                    name='o'+str(dest+1)))                                   
-p_var = []
-for dest in cols:
-    p_var.append([])
-    p_var[dest].append(mPrimal_Canonical_GUROBI.addVar(vtype=gbp.GRB.CONTINUOUS, 
-                                    name='p'+str(dest+1)))
-
-# Update Model
-mPrimal_Canonical_GUROBI.update()
-
-#Objective Function
-mPrimal_Canonical_GUROBI.setObjective(gbp.quicksum(Cj[dest]*desc_var[dest][0] 
-                        for dest in cols) +
-                        gbp.quicksum(Ej[dest]*o_var[dest][0] 
-                        for dest in cols) +
-                        gbp.quicksum(Gj[dest]*p_var[dest][0]
-                        for dest in cols), 
-                        gbp.GRB.MINIMIZE)
-
-# Constraints
-for orig in rows:
-    mPrimal_Canonical_GUROBI.addConstr(gbp.quicksum(Aij[orig][dest] *
-                        desc_var[dest][0] for dest in cols) + 
-                        gbp.quicksum(Oij[orig][dest] *
-                        o_var[dest][0]for dest in cols) - 
-                        Bi[orig] >= 2.5)
-for orig in rows:
-    mPrimal_Canonical_GUROBI.addConstr(gbp.quicksum(Aij[orig][dest] *
-                        desc_var[dest][0] for dest in cols) + 
-                        gbp.quicksum(Oij[orig][dest] *
-                        o_var[dest][0]for dest in cols) - 
-                        Bi[orig] <= 150.006)
-for orig in rows:
-    mPrimal_Canonical_GUROBI.addConstr(gbp.quicksum(
-                        desc_var[dest][0] for dest in cols) + 
-                        gbp.quicksum(
-                        o_var[dest][0]for dest in cols) >= 7.25)
-for orig in rows:
-    mPrimal_Canonical_GUROBI.addConstr(gbp.quicksum(
-                        desc_var[dest][0] for dest in cols) + 
-                        gbp.quicksum(
-                        o_var[dest][0]for dest in cols) <= 47.29)
-for orig in rows:
-    mPrimal_Canonical_GUROBI.addConstr(gbp.quicksum(Aij[orig][dest] *
-                        p_var[dest][0] for dest in cols) + 
-                        gbp.quicksum(
-                        o_var[dest][0]for dest in cols) / 
-                        Ej[orig] + Bi[orig] >= 1.7)
-
-# Optimize
-mPrimal_Canonical_GUROBI.optimize()
-# Write LP file
-mPrimal_Canonical_GUROBI.write('LP.lp')
-print '\n*************************************************************************'
-print '    |   Decision Variables'
-for v in mPrimal_Canonical_GUROBI.getVars():
-    print '    |  ', v.VarName, '=', v.x
-print '*************************************************************************'
-val = mPrimal_Canonical_GUROBI.objVal
-print '    |   Objective Value ------------------ ', val
-print '    |   Aij Sum -------------------------- ', AijSum
-print '    |   Oij Sum -------------------------- ', OijSum
-print '    |   Cj Sum --------------------------- ', CjSum
-print '    |   Ej Sum --------------------------- ', EjSum
-print '    |   Bi Sum --------------------------- ', BiSum
-print '    |   Matrix Dimensions ---------------- ', Aij.shape
-print '    |   Date/Time ------------------------ ', dt.datetime.now()
-print '*************************************************************************'
-print '-- Gurobi Canonical Primal Linear Programming Problem --'
-print '\nJames Gaboardi, 2015'
+    t1 = time.time()
+    
+    #  Constants
+    Aij = np.random.randint(5, 50, 250000)
+    Aij = Aij.reshape(500,500)
+    AijSum = np.sum(Aij)
+    Oij = np.random.randint(5, 10, 250000)
+    Oij = Oij.reshape(500,500)
+    OijSum = np.sum(Oij)
+    Cj = np.random.randint(10, 20, 500)
+    CjSum = np.sum(Cj)
+    Ej = np.random.randint(10, 20, 500)
+    EjSum = np.sum(Ej)
+    Gj = np.random.randint(10, 20, 500)
+    GjSum = np.sum(Gj)
+    Bi = np.random.randint(10, 20, 500)
+    BiSum = np.sum(Bi)
+    
+    # Matrix Shape
+    rows = range(len(Aij))
+    cols = range(len(Aij[0]))
+    
+    # Instantiate Model
+    mPrimal_Canonical_GUROBI = gbp.Model(' -- Canonical Primal Linear Programming Problem -- ')
+    
+    # Set Focus to Optimality
+    gbp.setParam('MIPFocus', 2)
+    
+    # Decision Variables
+    desc_var = []
+    for dest in cols:
+        desc_var.append([])
+        desc_var[dest].append(mPrimal_Canonical_GUROBI.addVar(vtype=gbp.GRB.CONTINUOUS, 
+                                        name='y'+str(dest+1)))                                   
+    o_var = []
+    for dest in cols:
+        o_var.append([])
+        o_var[dest].append(mPrimal_Canonical_GUROBI.addVar(vtype=gbp.GRB.CONTINUOUS, 
+                                        name='o'+str(dest+1)))                                   
+    p_var = []
+    for dest in cols:
+        p_var.append([])
+        p_var[dest].append(mPrimal_Canonical_GUROBI.addVar(vtype=gbp.GRB.CONTINUOUS, 
+                                        name='p'+str(dest+1)))
+    
+    # Update Model
+    mPrimal_Canonical_GUROBI.update()
+    
+    #Objective Function
+    mPrimal_Canonical_GUROBI.setObjective(gbp.quicksum(Cj[dest]*desc_var[dest][0] 
+                            for dest in cols) +
+                            gbp.quicksum(Ej[dest]*o_var[dest][0] 
+                            for dest in cols) +
+                            gbp.quicksum(Gj[dest]*p_var[dest][0]
+                            for dest in cols), 
+                            gbp.GRB.MINIMIZE)
+    
+    # Constraints
+    for orig in rows:
+        mPrimal_Canonical_GUROBI.addConstr(gbp.quicksum(Aij[orig][dest] *
+                            desc_var[dest][0] for dest in cols) + 
+                            gbp.quicksum(Oij[orig][dest] *
+                            o_var[dest][0]for dest in cols) - 
+                            Bi[orig] >= 2.5)
+    for orig in rows:
+        mPrimal_Canonical_GUROBI.addConstr(gbp.quicksum(Aij[orig][dest] *
+                            desc_var[dest][0] for dest in cols) + 
+                            gbp.quicksum(Oij[orig][dest] *
+                            o_var[dest][0]for dest in cols) - 
+                            Bi[orig] <= 150.006)
+    for orig in rows:
+        mPrimal_Canonical_GUROBI.addConstr(gbp.quicksum(
+                            desc_var[dest][0] for dest in cols) + 
+                            gbp.quicksum(
+                            o_var[dest][0]for dest in cols) >= 7.25)
+    for orig in rows:
+        mPrimal_Canonical_GUROBI.addConstr(gbp.quicksum(
+                            desc_var[dest][0] for dest in cols) + 
+                            gbp.quicksum(
+                            o_var[dest][0]for dest in cols) <= 47.29)
+    for orig in rows:
+        mPrimal_Canonical_GUROBI.addConstr(gbp.quicksum(Aij[orig][dest] *
+                            p_var[dest][0] for dest in cols) + 
+                            gbp.quicksum(
+                            o_var[dest][0]for dest in cols) / 
+                            Ej[orig] + Bi[orig] >= 1.7)
+    
+    # Optimize
+    try:
+        mPrimal_Canonical_GUROBI.optimize()
+    except Exception as e:
+        print e
+     
+    t2 = time.time() - t1       
+    # Write LP file
+    mPrimal_Canonical_GUROBI.write('LP.lp')
+    print '\n*************************************************************************'
+    print '    |   Decision Variables'
+    for v in mPrimal_Canonical_GUROBI.getVars():
+        print '    |  ', v.VarName, '=', v.x
+    print '*************************************************************************'
+    val = mPrimal_Canonical_GUROBI.objVal
+    print '    |   Objective Value ------------------ ', val
+    print '    |   Aij Sum -------------------------- ', AijSum
+    print '    |   Oij Sum -------------------------- ', OijSum
+    print '    |   Cj Sum --------------------------- ', CjSum
+    print '    |   Ej Sum --------------------------- ', EjSum
+    print '    |   Bi Sum --------------------------- ', BiSum
+    print '    |   Matrix Dimensions ---------------- ', Aij.shape
+    print '    |   Build & Solve(sec.) -------------- ', t2
+    print '    |   Date/Time ------------------------ ', dt.datetime.now()
+    print '*************************************************************************'
+    print '-- Gurobi Canonical Primal Linear Programming Problem --'
+    
+try:
+    GbpPrimCanCOMPLEX()
+except Exception as e:
+    print e
+else:
+    print '\nJames Gaboardi, 2015'
